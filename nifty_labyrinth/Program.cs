773e80fa5dab5665ed5f0816f62479f1db4185fb
unknown <lucy.gerrard@engineering.snow>
2025-11-06 09:04:32 -0700

    Solved

--- nifty_labyrinth/Program.cs
+++ nifty_labyrinth/Program.cs
@@ -6,11 +6,11 @@ using labyrinth;
  * do NOT edit the value of YourName. Changing YourName will change which
  * maze you get back, which might invalidate all your hard work!
 */
-string YOUR_NAME = "TODO: Replace this string with your name.";
+string YOUR_NAME = "Lucy Gerrard";
 
 /* Change these constants to contain the paths out of your mazes. */
-string PATH_OUT_OF_MAZE = "WNWSSSNNNEEESSWWSNEESW";
-string PATH_OUT_OF_TWISTY_MAZE = "SSEENNEWE";
+string PATH_OUT_OF_MAZE = "SEEENSWSESWWN";
+string PATH_OUT_OF_TWISTY_MAZE = "ESNSSNENS";
 
 MazeCell startLocation = MazeUtilities.mazeFor(YOUR_NAME);
 
@@ -32,10 +32,10 @@ MazeCell twistyStartLocation = MazeUtilities.twistyMazeFor(YOUR_NAME);
 
 if (MazeUtilities.isPathToFreedom(twistyStartLocation, PATH_OUT_OF_TWISTY_MAZE))
 {
-Console.WriteLine("Congratulations! You've found a way out of your twisty labyrinth.");
+    Console.WriteLine("Congratulations! You've found a way out of your twisty labyrinth.");
 }
 else
 {
-Console.WriteLine("Sorry, but you're still stuck in your twisty labyrinth.");
+    Console.WriteLine("Sorry, but you're still stuck in your twisty labyrinth.");
 }
 
